37b01bab33cf9db9ab32fabfddeebb7d3bf82305
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-07-13 22:52:17 +0600

    🔥 Remove logo and start experimenting with Flickr

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const serverId = '65535';
+  const id = '50062065332';
+  const secret = 'ef4c069d2d';
+  const flickrSrc = `https://live.staticflickr.com/${serverId}/${id}_${secret}_c.jpg`;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={flickrSrc} style={{ height: '80vmin' }} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
